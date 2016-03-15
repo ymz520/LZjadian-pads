eba6f859022cce95af5e8cfd7eb5fab63a2ffbbb
张荣桂 <yizhujianzhu@163.com>
2016-03-15 13:42:45 +0800

    Initial Commit

---
+++ LZjadian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZjadian
+//
+//  Created by 张 荣桂 on 16/3/15.
+//  Copyright © 2016年 张 荣桂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LZjadian/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LZjadian
+//
+//  Created by 张 荣桂 on 16/3/15.
+//  Copyright © 2016年 张 荣桂. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
